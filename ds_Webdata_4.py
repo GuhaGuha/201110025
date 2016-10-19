f98229ee891c388f2a53c05670126cefe2e39163
우한솔 <woo12242000@gmail.com>
2016-10-19 15:53:12 +0900

    Add files via upload

---
+++ ds_Webdata_4.py
@@ -0,0 +1,13 @@
+﻿import urllib
+keyword = '눈오는'
+f=urllib.urlopen("http://music.naver.com/search/search.nhn?query="+keyword+"&target=track")
+mydata = f.read()
+pos = mydata.find("트랙 리스트")
+pos1 = mydata.find("_title title NPI=",pos)
+mydata.find("title=", pos+20)
+import re
+p=re.compile('title=".*눈.?오는.*"')
+#res=p.search(data)
+res=p.findall(mydata)
+for item in res:
+    print item
---
+++ ds_Webdata_4_2.py
@@ -0,0 +1,25 @@
+﻿import lxml.html
+from lxml.cssselect import CSSSelector
+import lxml.html
+import requests
+
+keyword='눈오는'
+r = requests.get("http://music.naver.com/search/search.nhn?query="+keyword+"&x=0&y=0")
+
+_html = lxml.html.fromstring(r.text)
+sel = CSSSelector('table[summary] > tbody > ._tracklist_move')
+results = sel(_html)
+_selName = CSSSelector('.name > a.title')
+_selArtist = CSSSelector('._artist.artist')
+_selAlbum= CSSSelector('.album > a')
+for item in results:
+    #print lxml.html.tostring(item)
+    _name=_selName(item)
+    _artist=_selArtist(item)
+    _album=_selAlbum(item)
+    if _name:
+        print _artist[0].text_content().strip(),
+        print "---",
+        print _name[0].text_content(),
+        print "---",
+        print _album[0].text_content()
