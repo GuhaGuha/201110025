4e608210b84220ec7a5f0ce78e6652627ea9bee3
우한솔 <woo12242000@gmail.com>
2016-10-26 00:38:56 +0900

    Add files via upload

---
+++ ds_Webdata_5.py
@@ -0,0 +1,15 @@
+﻿import lxml.html
+from lxml.cssselect import CSSSelector
+import requests
+
+r = requests.get('https://www.ieee.org/conferences_events/index.html')
+html = lxml.html.fromstring(r.text)
+
+c=CSSSelector('#inner-container > div.content-gray > div.content-lc \
+        > div.content-lc-bottom > div.content-c > div:nth-child(1) > div \
+        > div a')
+nodes = sel(html)
+
+nodes = c(html)
+for node in nodes:
+    print node.text
